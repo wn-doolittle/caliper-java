e28a0a9a6108fe44258141ab491ade8b590c5ed3
Prashant Nayak <prashant@intellifylearning.com>
2014-06-16 20:44:54 -0400

    first cut of CaliperEvent along with proper GSON serialization values for fields

--- src/main/java/org/imsglobal/caliper/events/CaliperEvent.java
+++ src/main/java/org/imsglobal/caliper/events/CaliperEvent.java
@@ -3,6 +3,8 @@
  */
 package org.imsglobal.caliper.events;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Base class for all Caliper Events
  * 
@@ -11,7 +13,22 @@ package org.imsglobal.caliper.events;
  */
 public class CaliperEvent {
 
+	@SerializedName("@context")
+	private String context;
+
+	@SerializedName("@type")
+	private String type;
+
+	@SerializedName("@action")
 	private String action;
+
+	@SerializedName("@agent")
+	private String agent;
+	
+	@SerializedName("@activityContext")
+	private String activityContext;
+
+	@SerializedName("@startedAtTime")
 	private long startedAt;
 
 	/**
@@ -44,4 +61,63 @@ public class CaliperEvent {
 		this.startedAt = startedAt;
 	}
 
+	/**
+	 * @return the context
+	 */
+	public String getContext() {
+		return context;
+	}
+
+	/**
+	 * @param context
+	 *            the context to set
+	 */
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the agent
+	 */
+	public String getAgent() {
+		return agent;
+	}
+
+	/**
+	 * @param agent
+	 *            the agent to set
+	 */
+	public void setAgent(String agent) {
+		this.agent = agent;
+	}
+
+	/**
+	 * @return the activityContext
+	 */
+	public String getActivityContext() {
+		return activityContext;
+	}
+
+	/**
+	 * @param activityContext
+	 *            the activityContext to set
+	 */
+	public void setActivityContext(String activityContext) {
+		this.activityContext = activityContext;
+	}
 }
---
+++ src/test/java/org/imsglobal/caliper/events/CaliperEventTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.imsglobal.caliper.events;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+
+/**
+ * @author pnayak
+ * 
+ */
+public class CaliperEventTest {
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(CaliperEventTest.class);
+
+	private CaliperEvent caliperEvent;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+
+		caliperEvent = new CaliperEvent();
+		caliperEvent
+				.setContext("http://purl.imsglobal.org/ctx/caliper/v1/NavigationEvent");
+		caliperEvent.setType("NavigationEvent");
+		caliperEvent.setAction("navigate_to");
+		caliperEvent.setAgent("uri:/someEdu/user/42");
+		caliperEvent.setActivityContext("uri:/someEdu/reading/42");
+		caliperEvent.setStartedAt(1402965614516l);
+
+	}
+
+	@Test
+	public void test() {
+
+		String expectedCaliperEventJson = "{\"@context\":\"http://purl.imsglobal.org/ctx/caliper/v1/NavigationEvent\",\"@type\":\"NavigationEvent\",\"@action\":\"navigate_to\",\"@agent\":\"uri:/someEdu/user/42\",\"@activityContext\":\"uri:/someEdu/reading/42\",\"@startedAtTime\":1402965614516}";
+
+		Gson gson = new Gson();
+
+		String caliperEventJSON = gson.toJson(caliperEvent);
+
+		assertEquals("CaliperEvent JSON does not match expected JSON",
+				caliperEventJSON, expectedCaliperEventJson);
+
+		// LOG.debug("Caliper Event JSON = " + caliperEventJSON);
+	}
+}
