be5204cb5c16b73ba206b108d577b49b6a5b7b36
Prashant Nayak <prashant@intellifylearning.com>
2014-09-09 09:42:58 -0400

    fix action enum with proper actions from metric profile

--- src/main/java/org/imsglobal/caliper/events/assessment/AssessmentItemEvent.java
+++ src/main/java/org/imsglobal/caliper/events/assessment/AssessmentItemEvent.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreType;
 public class AssessmentItemEvent extends CaliperEvent {
 	@JsonIgnoreType
 	public enum Action {
-		started, paused, restarted, submitted
+		started, completed, viewed, reviewed, skipped
 	}
 
 	public AssessmentItemEvent() {
