531ec62897462cbca944ad13d0fd7056d8f90517
Anthony Whyte <anthwhyte@gmail.com>
2014-09-10 11:41:11 -0400

    Apply builder pattern to ActivityContext entity

--- src/main/java/org/imsglobal/caliper/entities/ActivityContext.java
+++ src/main/java/org/imsglobal/caliper/entities/ActivityContext.java
@@ -1,12 +1,77 @@
-/**
- * 
- */
 package org.imsglobal.caliper.entities;
 
-/**
- * @author pnayak
- *
- */
 public class ActivityContext extends CaliperEntity {
+    private final String type;
 
+    /**
+     * @param builder apply builder object properties to the Generated object.
+     */
+    protected ActivityContext(Builder<?> builder) {
+        super(builder);
+        this.type = builder.type;
+    }
+
+    /**
+     * @return the type
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Builder class provides a fluid interface for setting object properties.
+     * @param <T> builder
+     */
+    public static abstract class Builder<T extends Builder<T>> extends CaliperEntity.Builder<T>  {
+        private static final String ACTIVITYCONTEXT_TYPE = "http://purl.imsglobal.org/caliper/v1/ActivityContext";
+        private String type;
+
+        /**
+         * Initialize type with default value.  Required if builder().type() is not set by user.
+         */
+        public Builder() {
+            type(ACTIVITYCONTEXT_TYPE);
+        }
+
+        /**
+         * @param type
+         * @return the IMS Global type reference URI.
+         */
+        @Override
+        public T type(String type) {
+            if (type.equals(ACTIVITYCONTEXT_TYPE)) {
+                this.type = type;
+            } else {
+                this.type = ACTIVITYCONTEXT_TYPE;
+            }
+            return self();
+        }
+
+        /**
+         * Client invokes build method in order to create an immutable object.
+         * @return a new instance of Generated.
+         */
+        public ActivityContext build() {
+            return new ActivityContext(this);
+        }
+    }
+
+    /**
+     *
+     */
+    private static class Builder2 extends Builder<Builder2> {
+        @Override
+        protected Builder2 self() {
+            return this;
+        }
+    }
+
+    /**
+     * Static factory method.
+     * @return a new instance of the builder.
+     */
+    public static Builder<?> builder() {
+        return new Builder2();
+    }
 }
