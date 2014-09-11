d19390391ffcffe96f6303b5e402bdd5be53fd8c
Anthony Whyte <anthwhyte@gmail.com>
2014-09-11 17:24:08 -0400

    Change method signature to get/set CaliperAssignableDigitalResource object

--- src/main/java/org/imsglobal/caliper/profiles/AssignableProfile.java
+++ src/main/java/org/imsglobal/caliper/profiles/AssignableProfile.java
@@ -1,116 +1,24 @@
 package org.imsglobal.caliper.profiles;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.imsglobal.caliper.actions.AssignableActions;
+import org.imsglobal.caliper.entities.assignable.CaliperAssignableDigitalResource;
 
-import java.util.Date;
-import java.util.ResourceBundle;
-
-@JsonPropertyOrder({
-    "dateCreated",
-    "datePublished",
-    "dateToStartOn",
-    "dateToShow",
-    "dateToSubmit",
-    "maxAttempts",
-    "maxSubmits",
-    "action" })
 public class AssignableProfile extends BaseProfile {
 
-    @JsonProperty("dateCreated")
-    private Date dateCreated;
-
-    @JsonProperty("datePublished")
-    private Date datePublished;
-
-    @JsonProperty("dateToStartOn")
-    private Date dateToStartOn;
-
-    @JsonProperty("dateToShow")
-    private Date dateToShow;
-
-    @JsonProperty("dateToSubmit")
-    private Date dateToSubmit;
-
-    @JsonProperty("maxAttempts")
-    private int maxAttempts;
-
-    @JsonProperty("maxSubmits")
-    private int maxSubmits;
-
-    @JsonProperty("action")
-    private String action;
+    private CaliperAssignableDigitalResource assignable;
 
     /**
      * @param builder apply builder object properties to the profile object.
      */
     protected AssignableProfile(Builder<?> builder) {
         super(builder);
-        this.dateCreated = builder.dateCreated;
-        this.datePublished = builder.datePublished;
-        this.dateToStartOn = builder.dateToStartOn;
-        this.dateToShow = builder.dateToShow;
-        this.dateToSubmit = builder.dateToSubmit;
-        this.maxAttempts = builder.maxAttempts;
-        this.maxSubmits = builder.maxSubmits;
-        this.action = builder.action;
+        this.assignable = builder.assignable;
     }
 
     /**
-     * @return creation date.
+     * @return assignable digital resource.
      */
-    public Date getDateCreated() {
-        return dateCreated;
-    }
-
-    /**
-     * @return publication date.
-     */
-    public Date getDatePublished() {
-        return datePublished;
-    }
-
-    /**
-     * @return start date.
-     */
-    public Date getDateToStartOn() {
-        return dateToStartOn;
-    }
-
-    /**
-     * @return show date.
-     */
-    public Date getDateToShow() {
-        return dateToShow;
-    }
-
-    /**
-     * @return submission date.
-     */
-    public Date getDateToSubmit() {
-        return dateToSubmit;
-    }
-
-    /**
-     * @return max attempts permitted.
-     */
-    public int getMaxAttempts() {
-        return maxAttempts;
-    }
-
-    /**
-     * @return max submissions permitted.
-     */
-    public int getMaxSubmits() {
-        return maxSubmits;
-    }
-
-    /**
-     * @return action.
-     */
-    public String getAction() {
-        return action;
+    public CaliperAssignableDigitalResource getAssignable() {
+        return assignable;
     }
 
     /**
@@ -118,84 +26,14 @@ public class AssignableProfile extends BaseProfile {
      * @param <T> builder
      */
     public static abstract class Builder<T extends Builder<T>> extends BaseProfile.Builder<T>  {
-        private Date dateCreated;
-        private Date datePublished;
-        private Date dateToStartOn;
-        private Date dateToShow;
-        private Date dateToSubmit;
-        private int maxAttempts;
-        private int maxSubmits;
-        private String action;
+        private CaliperAssignableDigitalResource assignable;
 
         /**
-         * @param dateCreated
+         * @param assignable
          * @return builder.
          */
-        private T dateCreated(Date dateCreated) {
-            this.dateCreated = dateCreated;
-            return self();
-        }
-
-        /**
-         * @param datePublished
-         * @return builder.
-         */
-        private T datePublished(Date datePublished) {
-            this.datePublished = datePublished;
-            return self();
-        }
-
-        /**
-         * @param dateToStartOn
-         * @return builder.
-         */
-        private T dateToStartOn(Date dateToStartOn) {
-            this.dateToStartOn = dateToStartOn;
-            return self();
-        }
-
-        /**
-         * @param dateToShow
-         * @return builder.
-         */
-        private T dateToShow(Date dateToShow) {
-            this.dateToShow = dateToShow;
-            return self();
-        }
-
-        /**
-         * @param dateToSubmit
-         * @return builder.
-         */
-        private T dateToSubmit(Date dateToSubmit) {
-            this.dateToSubmit = dateToSubmit;
-            return self();
-        }
-
-        /**
-         * @param maxAttempts
-         * @return builder.
-         */
-        private T maxAttempts(int maxAttempts) {
-            this.maxAttempts = maxAttempts;
-            return self();
-        }
-
-        /**
-         * @param maxSubmits
-         * @return builder.
-         */
-        private T maxSubmits(int maxSubmits) {
-            this.maxSubmits = maxSubmits;
-            return self();
-        }
-
-        /**
-         * @param key
-         * @return builder after validating action key.
-         */
-        public T action(String key) {
-            this.action = validateAction(key);
+        private T assignable(CaliperAssignableDigitalResource assignable) {
+            this.assignable = assignable;
             return self();
         }
 
@@ -224,17 +62,5 @@ public class AssignableProfile extends BaseProfile {
      */
     public static Builder<?> builder() {
         return new Builder2();
-    }
-
-    /**
-     * @param key resource bundle key attribute of target constant
-     * @return resource bundle key
-     */
-    private static String validateAction(String key) {
-        if (AssignableActions.hasKey(key)) {
-            return ResourceBundle.getBundle("resources.actions").getString(key);
-        } else {
-            throw new IllegalArgumentException("Unrecognized constant");
-        }
     }
 }
