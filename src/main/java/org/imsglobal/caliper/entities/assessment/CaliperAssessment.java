783d0a0ee7e7e115b36effbec6cb0c73d77a1a7b
Anthony Whyte <anthwhyte@gmail.com>
2014-09-11 11:21:40 -0400

    Add builders to Assessment entities

--- src/main/java/org/imsglobal/caliper/entities/assessment/CaliperAssessment.java
+++ src/main/java/org/imsglobal/caliper/entities/assessment/CaliperAssessment.java
@@ -1,26 +1,86 @@
-/**
- * 
- */
 package org.imsglobal.caliper.entities.assessment;
 
 import org.imsglobal.caliper.entities.assignable.CaliperAssignableDigitalResource;
 import org.imsglobal.caliper.entities.qti.Assessment;
 
 /**
- * @author pnayak
+ * Caliper representation of an Assessment.
  * 
- *         Caliper representation of an Assessment.
- * 
- *         Part of the Assessment Metric Profile
- * 
+ * Part of the Assessment Metric Profile
  */
-public class CaliperAssessment extends CaliperAssignableDigitalResource
-		implements Assessment {
+public class CaliperAssessment extends CaliperAssignableDigitalResource implements Assessment {
 
-	public CaliperAssessment(String id) {
-		super(id);
-		setId(id);
-		setType("http://purl.imsglobal.org/caliper/v1/Assessment");
-	}
+    private final String type;
 
+    /**
+     * @param builder apply builder object properties to the CaliperAssessment object.
+     */
+    protected CaliperAssessment(Builder<?> builder) {
+        super(builder);
+        this.type = builder.type;
+    }
+
+    /**
+     * @return the type
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Builder class provides a fluid interface for setting object properties.
+     * @param <T> builder
+     */
+    public static abstract class Builder<T extends Builder<T>> extends CaliperAssignableDigitalResource.Builder<T>  {
+        private static final String ASSESSMENT_TYPE = "http://purl.imsglobal.org/caliper/v1/Assessment";
+        private String type;
+
+        /**
+         * Initialize type with default value.  Required if builder().type() is not set by user.
+         */
+        public Builder() {
+            type(ASSESSMENT_TYPE);
+        }
+
+        /**
+         * @param type
+         * @return the IMS Global type reference URI.
+         */
+        @Override
+        public T type(String type) {
+            if (type.equals(ASSESSMENT_TYPE)) {
+                this.type = type;
+            } else {
+                this.type = ASSESSMENT_TYPE;
+            }
+            return self();
+        }
+
+        /**
+         * Client invokes build method in order to create an immutable object.
+         * @return a new instance of CaliperAssessment.
+         */
+        public CaliperAssessment build() {
+            return new CaliperAssessment(this);
+        }
+    }
+
+    /**
+     *
+     */
+    private static class Builder2 extends Builder<Builder2> {
+        @Override
+        protected Builder2 self() {
+            return this;
+        }
+    }
+
+    /**
+     * Static factory method.
+     * @return a new instance of the builder.
+     */
+    public static Builder<?> builder() {
+        return new Builder2();
+    }
 }
--- src/main/java/org/imsglobal/caliper/entities/assessment/CaliperAssessmentItem.java
+++ src/main/java/org/imsglobal/caliper/entities/assessment/CaliperAssessmentItem.java
@@ -7,20 +7,83 @@ import org.imsglobal.caliper.entities.assignable.CaliperAssignableDigitalResourc
 import org.imsglobal.caliper.entities.qti.Assessment;
 
 /**
- * @author pnayak
+ * Caliper representation of an Assessment Item.
  * 
- *         Caliper representation of an Assessment Item.
- * 
- *         Part of the Assessment Metric Profile
- * 
+ * Part of the Assessment Metric Profile.
  */
-public class CaliperAssessmentItem extends CaliperAssignableDigitalResource
-		implements Assessment {
+public class CaliperAssessmentItem extends CaliperAssignableDigitalResource implements Assessment {
 
-	public CaliperAssessmentItem(String id) {
-		super(id);
-		setId(id);
-		setType("http://purl.imsglobal.org/caliper/v1/AssessmentItem");
-	}
+    private final String type;
 
+    /**
+     * @param builder apply builder object properties to the CaliperAssessmentItem object.
+     */
+    protected CaliperAssessmentItem(Builder<?> builder) {
+        super(builder);
+        this.type = builder.type;
+    }
+
+    /**
+     * @return the type
+     */
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Builder class provides a fluid interface for setting object properties.
+     * @param <T> builder
+     */
+    public static abstract class Builder<T extends Builder<T>> extends CaliperAssignableDigitalResource.Builder<T>  {
+        private static final String ASSESSMENTITEM_TYPE = "http://purl.imsglobal.org/caliper/v1/AssessmentItem";
+        private String type;
+
+        /**
+         * Initialize type with default value.  Required if builder().type() is not set by user.
+         */
+        public Builder() {
+            type(ASSESSMENTITEM_TYPE);
+        }
+
+        /**
+         * @param type
+         * @return the IMS Global type reference URI.
+         */
+        @Override
+        public T type(String type) {
+            if (type.equals(ASSESSMENTITEM_TYPE)) {
+                this.type = type;
+            } else {
+                this.type = ASSESSMENTITEM_TYPE;
+            }
+            return self();
+        }
+
+        /**
+         * Client invokes build method in order to create an immutable object.
+         * @return a new instance of CaliperAssessmentItem.
+         */
+        public CaliperAssessmentItem build() {
+            return new CaliperAssessmentItem(this);
+        }
+    }
+
+    /**
+     *
+     */
+    private static class Builder2 extends Builder<Builder2> {
+        @Override
+        protected Builder2 self() {
+            return this;
+        }
+    }
+
+    /**
+     * Static factory method.
+     * @return a new instance of the builder.
+     */
+    public static Builder<?> builder() {
+        return new Builder2();
+    }
 }
