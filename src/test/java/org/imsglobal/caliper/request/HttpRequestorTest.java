5dc851d617014ca2e7fb83ff65ae0492e6c234b3
Anthony Whyte <anthwhyte@gmail.com>
2014-10-16 17:22:40 -0400

    Fix tests to work with builders

--- src/test/java/org/imsglobal/caliper/request/HttpRequestorTest.java
+++ src/test/java/org/imsglobal/caliper/request/HttpRequestorTest.java
@@ -1,8 +1,5 @@
 package org.imsglobal.caliper.request;
 
-import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
-import static org.junit.Assert.assertEquals;
-
 import org.apache.http.entity.StringEntity;
 import org.imsglobal.caliper.Options;
 import org.imsglobal.caliper.TestUtils;
@@ -15,63 +12,59 @@ import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/**
- * Test HttpRequestor
- * 
- * @author pnayak
- * @author yoganandp
- */
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.junit.Assert.assertEquals;
+
 @Category(org.imsglobal.caliper.UnitTest.class)
 public class HttpRequestorTest {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(HttpRequestorTest.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(HttpRequestorTest.class);
 
-	CaliperEvent caliperEvent;
-	Options options;
-	HttpRequestor httpRequestor;
+    CaliperEvent caliperEvent;
+    Options options;
+    HttpRequestor httpRequestor;
 
-	@Before
-	public void setup() {
+    @Before
+    public void setup() {
 
-		caliperEvent = TestUtils.getTestCaliperEvent();
+        caliperEvent = TestUtils.getTestNavigationEvent();
 
-		httpRequestor = new HttpRequestor(TestUtils.getTestingOptions());
-	}
+        httpRequestor = new HttpRequestor(TestUtils.getTestingOptions());
+    }
 
-	@Test
-	public void testGeneratePayloadJson() throws Exception {
+    @Test
+    public void testGeneratePayloadJson() throws Exception {
 
-		String jsonPayload = null;
+        String jsonPayload = null;
 
-		jsonPayload = httpRequestor.getPayloadJson(caliperEvent,
-				"caliper-java_fccffd9b-68d5-4183-b563-e22136aafaa3",
-				DateTime.parse("2014-07-01T14:29:29.858-04:00"));
-		
-		LOG.debug("JSON payload: " + jsonPayload);
+        jsonPayload = httpRequestor.getPayloadJson(caliperEvent,
+                "caliper-java_fccffd9b-68d5-4183-b563-e22136aafaa3",
+                DateTime.parse("2014-07-01T14:29:29.858-04:00"));
 
-		assertEquals("Test HTTP Requestor payload JSON",
-				jsonFixture("fixtures/eventStorePayload.json"), jsonPayload);
+        LOG.debug("JSON payload: " + jsonPayload);
 
-	}
+        assertEquals("Test HTTP Requestor payload JSON",
+                jsonFixture("fixtures/eventStorePayload.json"), jsonPayload);
 
-	@Test
-	public void testGeneratePayloadContentType() throws Exception {
+    }
 
-		StringEntity payload = null;
+    @Test
+    public void testGeneratePayloadContentType() throws Exception {
 
-		payload = httpRequestor.generatePayload(caliperEvent,
-				"caliper-java_fccffd9b-68d5-4183-b563-e22136aafaa3",
-				DateTime.parse("2014-07-01T14:29:29.858-04:00"));
+        StringEntity payload = null;
 
-		String expectedContentType = "Content-Type: application/json";
-		assertEquals(expectedContentType, payload.getContentType().toString());
-	}
+        payload = httpRequestor.generatePayload(caliperEvent,
+                "caliper-java_fccffd9b-68d5-4183-b563-e22136aafaa3",
+                DateTime.parse("2014-07-01T14:29:29.858-04:00"));
 
-	@After
-	public void teardown() {
-		caliperEvent = null;
-		options = null;
-	}
+        String expectedContentType = "Content-Type: application/json";
+        assertEquals(expectedContentType, payload.getContentType().toString());
+    }
 
+    @After
+    public void teardown() {
+        caliperEvent = null;
+        options = null;
+    }
 }
