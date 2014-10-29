3384c9853cfaafdb5f76941eac668c59c4bb7cc8
Anthony Whyte <anthwhyte@gmail.com>
2014-10-29 13:40:32 -0400

    #21 MediaObject interface should extend CreativeWork interface; plus tidy up comments

--- src/main/java/org/imsglobal/caliper/entities/schemadotorg/MediaObject.java
+++ src/main/java/org/imsglobal/caliper/entities/schemadotorg/MediaObject.java
@@ -1,19 +1,14 @@
-/**
- * 
- */
 package org.imsglobal.caliper.entities.schemadotorg;
 
 /**
- * @author pnayak
- * 
- *         From https://schema.org/MediaObject
- * 
- *         An image, video, or audio object embedded in a web page. Note that a
- *         creative work may have many media objects associated with it on the
- *         same web page. For example, a page about a single song
- *         (MusicRecording) may have a music video (VideoObject), and a high and
- *         low bandwidth audio stream (2 AudioObject's).
+ * An image, video, or audio object embedded in a web page. Note that a
+ * creative work may have many media objects associated with it on the
+ * same web page. For example, a page about a single song (MusicRecording)
+ * may have a music video (VideoObject), and a high and low bandwidth audio
+ * stream (2 AudioObject's).
+ *
+ * From https://schema.org/MediaObject
  */
-public interface MediaObject {
+public interface MediaObject extends CreativeWork {
 
 }
