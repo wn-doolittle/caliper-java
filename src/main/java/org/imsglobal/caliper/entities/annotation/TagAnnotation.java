4e7df2521a49b2c8006c996146dbd059352f4ff7
Anthony Whyte <anthwhyte@gmail.com>
2014-11-06 13:04:15 -0500

    Add TagAnnotationEvent unit test

--- src/main/java/org/imsglobal/caliper/entities/annotation/TagAnnotation.java
+++ src/main/java/org/imsglobal/caliper/entities/annotation/TagAnnotation.java
@@ -1,10 +1,12 @@
 package org.imsglobal.caliper.entities.annotation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.google.common.collect.Lists;
 
 import java.util.List;
 
+@JsonPropertyOrder({ "@id", "@type", "name", "properties", "lastModifiedTime", "target", "tags" })
 public class TagAnnotation extends org.imsglobal.caliper.entities.annotation.Annotation {
 
     @JsonProperty("@type")
