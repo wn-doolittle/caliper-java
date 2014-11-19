46d035a9b7b175b469b10f5e9cb0ca1e56b8e82f
Anthony Whyte <anthwhyte@gmail.com>
2014-11-19 15:27:06 -0500

    WebPage missing type getter; update tests; delete obsolete caliperEvent.json

--- src/test/java/org/imsglobal/caliper/TestUtils.java
+++ src/test/java/org/imsglobal/caliper/TestUtils.java
@@ -305,29 +305,29 @@ public class TestUtils {
 
         profile.getActions().add(AnnotationActions.SHARED.key());
         profile.getAnnotations().add(SharedAnnotation.builder()
-                .id("https://someEduApp.edu/shared/9999")
-                .withAgents(Lists.<Agent>newArrayList(
-                    Person.builder()
-                        .id("https://some-university.edu/students/657585")
-                        .lastModifiedTime(1402965614516l)
-                        .build(),
-                    Person.builder()
-                        .id("https://some-university.edu/students/667788")
-                        .lastModifiedTime(1402965614516l)
-                        .build()))
-                .target(Frame.builder()
-                    .id("https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3/3)")
-                    .name("Key Figures: John Adams")
-                    .partOf(EpubVolume.builder()
-                        .id("https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3)")
-                        .name("The Glorious Cause: The American Revolution, 1763-1789 (Oxford History of the United States)")
-                        .lastModifiedTime(1402965614516l)
-                        .build())
+            .id("https://someEduApp.edu/shared/9999")
+            .withAgents(Lists.<Agent>newArrayList(
+                Person.builder()
+                    .id("https://some-university.edu/students/657585")
                     .lastModifiedTime(1402965614516l)
-                    .index(3)
-                    .build())  // TODO: REDUNDANT PROPERTY?
+                    .build(),
+                Person.builder()
+                    .id("https://some-university.edu/students/667788")
+                    .lastModifiedTime(1402965614516l)
+                    .build()))
+            .target(Frame.builder()
+                .id("https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3/3)")
+                .name("Key Figures: John Adams")
+                .partOf(EpubVolume.builder()
+                    .id("https://github.com/readium/readium-js-viewer/book/34843#epubcfi(/4/3)")
+                    .name("The Glorious Cause: The American Revolution, 1763-1789 (Oxford History of the United States)")
+                    .lastModifiedTime(1402965614516l)
+                    .build())
                 .lastModifiedTime(1402965614516l)
-                .build());
+                .index(3)
+                .build())  // TODO: REDUNDANT PROPERTY?
+            .lastModifiedTime(1402965614516l)
+            .build());
         profile.getTargets().add(Iterables.getLast(profile.getAnnotations()).getTarget());
 
         return profile;
