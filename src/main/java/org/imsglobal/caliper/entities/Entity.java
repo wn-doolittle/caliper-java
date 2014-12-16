a3abf29b17bc60f325faca3f100ddca705c642f3
Anthony Whyte <anthwhyte@gmail.com>
2014-12-16 11:45:37 -0500

    Remove Entity.properties; update fixtures

--- src/main/java/org/imsglobal/caliper/entities/Entity.java
+++ src/main/java/org/imsglobal/caliper/entities/Entity.java
@@ -2,10 +2,6 @@ package org.imsglobal.caliper.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-
-import java.util.Map;
 
 /**
  * The base Caliper Entity.  Analogous to a schema.org Thing.
@@ -53,9 +49,6 @@ public abstract class Entity {
     @JsonProperty("name")
     private final String name;
 
-    @JsonProperty("properties")
-    private final ImmutableMap<String, Object> properties;
-
     @JsonProperty("lastModifiedTime")
     private final long lastModifiedTime;
 
@@ -66,9 +59,6 @@ public abstract class Entity {
         this.id = builder.id;
         this.type = builder.type;
         this.name = builder.name;
-        this.properties = ImmutableMap.copyOf(builder.properties);
-        //this.properties = ImmutableMap.<String, Object>builder().putAll(builder.properties).build();
-        //this.properties = Collections.unmodifiableMap(properties);
         this.lastModifiedTime = builder.lastModifiedTime;
     }
 
@@ -94,14 +84,6 @@ public abstract class Entity {
     }
 
     /**
-     * Return an immutable view of the properties map.
-     * @return properties.
-     */
-    public ImmutableMap<String, Object> getProperties() {
-        return properties;
-    }
-
-    /**
      * @return the lastModifiedTime.
      */
     public long getLastModifiedTime() {
@@ -116,7 +98,6 @@ public abstract class Entity {
         private String id;
         private String type;
         private String name;
-        private Map<String, Object> properties = Maps.newHashMap();
         private long lastModifiedTime;
 
         protected abstract T self();
@@ -152,25 +133,6 @@ public abstract class Entity {
          */
         public T name(String name) {
             this.name = name;
-            return self();
-        }
-
-        /**
-         * @param properties
-         * @return builder.
-         */
-        public T properties(Map<String, Object> properties) {
-            this.properties = properties;
-            return self();
-        }
-
-        /**
-         * @param key
-         * @param value
-         * @return builder.
-         */
-        public T property(String key, Object value) {
-            this.properties.put(key, value);
             return self();
         }
 
