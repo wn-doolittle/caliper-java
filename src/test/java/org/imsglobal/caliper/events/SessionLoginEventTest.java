939a16d31451255ca7d2a889bea669a73c887d3c
Anthony Whyte <anthwhyte@gmail.com>
2015-01-27 16:38:27 -0500

    Rename fixture (present tense name is the format); update test

--- src/test/java/org/imsglobal/caliper/events/SessionLoginEventTest.java
+++ src/test/java/org/imsglobal/caliper/events/SessionLoginEventTest.java
@@ -54,6 +54,6 @@ public class SessionLoginEventTest {
     @Test
     public void caliperEventSerializesToJSON() throws Exception {
         assertEquals("Test if loggedIn event is serialized to JSON with expected values",
-                jsonFixture("fixtures/caliperSessionLoggedInEvent.json"), asJson(event));
+                jsonFixture("fixtures/caliperSessionLogInEvent.json"), asJson(event));
     }
 }
