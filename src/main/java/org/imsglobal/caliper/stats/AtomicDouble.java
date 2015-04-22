abe416b809f672bcfb0b341685c21bfff72e857d
Anthony Whyte <anthwhyte@gmail.com>
2015-04-22 15:34:00 -0400

    Add License Header

--- src/main/java/org/imsglobal/caliper/stats/AtomicDouble.java
+++ src/main/java/org/imsglobal/caliper/stats/AtomicDouble.java
@@ -1,8 +1,26 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.imsglobal.caliper.stats;
 
-import static java.lang.Double.doubleToLongBits;
-
 import java.util.concurrent.atomic.AtomicLong;
+
+import static java.lang.Double.doubleToLongBits;
 
 /**
  * A thread-safe and lock-free double implementation
@@ -10,58 +28,58 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 class AtomicDouble extends Number {
 
-	private static final long serialVersionUID = -2480549991498013056L;
-	private AtomicLong bits;
+    private static final long serialVersionUID = -2480549991498013056L;
+    private AtomicLong bits;
 
-	public AtomicDouble() {
-		this(0);
-	}
+    public AtomicDouble() {
+        this(0);
+    }
 
-	public AtomicDouble(double initialValue) {
-		bits = new AtomicLong(doubleToLongBits(initialValue));
-	}
+    public AtomicDouble(double initialValue) {
+        bits = new AtomicLong(doubleToLongBits(initialValue));
+    }
 
-	public final boolean compareAndSet(double expect, double update) {
-		return bits.compareAndSet(doubleToLongBits(expect),
-				doubleToLongBits(update));
-	}
+    public final boolean compareAndSet(double expect, double update) {
+        return bits.compareAndSet(doubleToLongBits(expect),
+                doubleToLongBits(update));
+    }
 
-	public final void set(double newValue) {
-		bits.set(doubleToLongBits(newValue));
-	}
+    public final void set(double newValue) {
+        bits.set(doubleToLongBits(newValue));
+    }
 
-	public final double get() {
+    public final double get() {
 
-		return Double.longBitsToDouble(bits.get());
-	}
+        return Double.longBitsToDouble(bits.get());
+    }
 
-	public float floatValue() {
-		return (float)get();
-	}
+    public float floatValue() {
+        return (float)get();
+    }
 
-	public final double addAndGet(double newValue) {
-		return Double.longBitsToDouble(bits.addAndGet(doubleToLongBits(newValue)));
-	}
+    public final double addAndGet(double newValue) {
+        return Double.longBitsToDouble(bits.addAndGet(doubleToLongBits(newValue)));
+    }
 
-	public final double getAndSet(double newValue) {
-		return Double.longBitsToDouble(bits.getAndSet(doubleToLongBits(newValue)));
-	}
+    public final double getAndSet(double newValue) {
+        return Double.longBitsToDouble(bits.getAndSet(doubleToLongBits(newValue)));
+    }
 
-	public final boolean weakCompareAndSet(float expect, float update) {
-		return bits.weakCompareAndSet(doubleToLongBits(expect),
-				doubleToLongBits(update));
-	}
+    public final boolean weakCompareAndSet(float expect, float update) {
+        return bits.weakCompareAndSet(doubleToLongBits(expect),
+                doubleToLongBits(update));
+    }
 
-	public double doubleValue() {
-		return (double) floatValue();
-	}
+    public double doubleValue() {
+        return (double) floatValue();
+    }
 
-	public int intValue() {
-		return (int) get();
-	}
+    public int intValue() {
+        return (int) get();
+    }
 
-	public long longValue() {
-		return (long) get();
-	}
+    public long longValue() {
+        return (long) get();
+    }
 
 }
--- src/main/java/org/imsglobal/caliper/stats/StatisticsMap.java
+++ src/main/java/org/imsglobal/caliper/stats/StatisticsMap.java
@@ -1,45 +1,63 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.imsglobal.caliper.stats;
 
 import java.util.concurrent.ConcurrentHashMap;
 
 public class StatisticsMap extends ConcurrentHashMap<String, Statistic> {
 
-	private static final long serialVersionUID = -8837006750327885446L;
-	
-	public Statistic ensure(String key) {
-		if (this.containsKey(key)) return this.get(key);
+    private static final long serialVersionUID = -8837006750327885446L;
 
-		Statistic statistic = new Statistic();
-		this.put(key, statistic);
-		return statistic;
-	}
-	
-	public void update(String operation, double val) {
-		if (!this.containsKey(operation)) {
-			this.putIfAbsent(operation, new Statistic());
-		}
-		
-		this.get(operation).update(val);
-	}
-	
-	@Override
-	public String toString() {
-		
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("\n-------- Caliper Java Statistics --------\n");
-		
-		for (Entry<String, Statistic> entry : entrySet()) {
-			
-			String operation = entry.getKey();
-			Statistic statistic = entry.getValue();
-			
-			builder.append(String.format("%s : %s\n", operation, statistic.toString()));
-		}
-		
-		builder.append("----------------------------------------\n");
-		
-		return builder.toString();
-	}
-	
+    public Statistic ensure(String key) {
+        if (this.containsKey(key)) return this.get(key);
+
+        Statistic statistic = new Statistic();
+        this.put(key, statistic);
+        return statistic;
+    }
+
+    public void update(String operation, double val) {
+        if (!this.containsKey(operation)) {
+            this.putIfAbsent(operation, new Statistic());
+        }
+
+        this.get(operation).update(val);
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("\n-------- Caliper Java Statistics --------\n");
+
+        for (Entry<String, Statistic> entry : entrySet()) {
+
+            String operation = entry.getKey();
+            Statistic statistic = entry.getValue();
+
+            builder.append(String.format("%s : %s\n", operation, statistic.toString()));
+        }
+
+        builder.append("----------------------------------------\n");
+
+        return builder.toString();
+    }
+
 }
