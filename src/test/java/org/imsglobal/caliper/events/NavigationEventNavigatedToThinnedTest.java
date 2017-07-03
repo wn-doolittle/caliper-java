3e3426a687b6c50cbc07208e1f38d5d8b8e856dc
Anthony Whyte <anthwhyte@gmail.com>
2017-07-02 22:31:03 -0400

    Add NavigationEventNavigatedToThinnedTest

---
+++ src/test/java/org/imsglobal/caliper/events/NavigationEventNavigatedToThinnedTest.java
@@ -0,0 +1,146 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.imsglobal.caliper.events;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.imsglobal.caliper.actions.Action;
+import org.imsglobal.caliper.context.JsonldContext;
+import org.imsglobal.caliper.context.JsonldStringContext;
+import org.imsglobal.caliper.databind.JxnCoercibleSimpleModule;
+import org.imsglobal.caliper.entities.agent.CourseSection;
+import org.imsglobal.caliper.entities.agent.Membership;
+import org.imsglobal.caliper.entities.agent.Person;
+import org.imsglobal.caliper.entities.agent.SoftwareApplication;
+import org.imsglobal.caliper.entities.resource.WebPage;
+import org.imsglobal.caliper.entities.session.Session;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+
+@Category(org.imsglobal.caliper.UnitTest.class)
+public class NavigationEventNavigatedToThinnedTest {
+    private JsonldContext context;
+    private String id;
+    private Person actor;
+    private WebPage object;
+    private SoftwareApplication edApp;
+    private CourseSection group;
+    private Membership membership;
+    private WebPage referrer;
+    private Session session;
+    private NavigationEvent event;
+
+    private static final String BASE_IRI = "https://example.edu";
+    private static final String SECTION_IRI = BASE_IRI.concat("/terms/201601/courses/7/sections/1");
+
+    @Before
+    public void setUp() throws Exception {
+        context = JsonldStringContext.getDefault();
+
+        id = "urn:uuid:71657137-8e6e-44f8-8499-e1c3df6810d2";
+
+        actor = Person.builder().id(BASE_IRI.concat("/users/554433")).coercedToId(true).build();
+
+        object = WebPage.builder()
+            .id(SECTION_IRI.concat("/pages/2"))
+            .coercedToId(true)
+            .build();
+
+        referrer = WebPage.builder()
+            .id(SECTION_IRI.concat("/pages/1"))
+            .coercedToId(true)
+            .build();
+
+        edApp = SoftwareApplication.builder().id(BASE_IRI).coercedToId(true).build();
+
+        group = CourseSection.builder().id(SECTION_IRI).coercedToId(true).build();
+
+        membership = Membership.builder()
+            .id(SECTION_IRI.concat("/rosters/1"))
+            .coercedToId(true)
+            .build();
+
+        session = Session.builder()
+            .id(BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259"))
+            .coercedToId(true)
+            .build();
+
+        // Build event
+        event = buildEvent(Action.NAVIGATED_TO);
+    }
+
+    @Test
+    public void caliperEventSerializesToJSON() throws Exception {
+        SimpleFilterProvider provider = new SimpleFilterProvider()
+            .setFailOnUnknownId(true);
+
+        ObjectMapper mapper = new ObjectMapper()
+            .setDateFormat(new ISO8601DateFormat())
+            .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+            .setFilterProvider(provider)
+            .registerModules(new JodaModule(), new JxnCoercibleSimpleModule());
+
+        String json = mapper.writeValueAsString(event);
+
+        String fixture = jsonFixture("fixtures/caliperEventNavigationNavigatedToThinned.json");
+        JSONAssert.assertEquals(fixture, json, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void navigationEventRejectsViewedAction() {
+        buildEvent(Action.VIEWED);
+    }
+
+    @After
+    public void teardown() {
+        event = null;
+    }
+
+    /**
+     * Build Navigation event
+     * @param action
+     * @return event
+     */
+    private NavigationEvent buildEvent(Action action) {
+        return NavigationEvent.builder()
+            .context(context)
+            .id(id)
+            .actor(actor)
+            .action(action)
+            .object(object)
+            .eventTime(new DateTime(2016, 11, 15, 10, 15, 0, 0, DateTimeZone.UTC))
+            .referrer(referrer)
+            .edApp(edApp)
+            .group(group)
+            .membership(membership)
+            .session(session)
+            .build();
+    }
+}
