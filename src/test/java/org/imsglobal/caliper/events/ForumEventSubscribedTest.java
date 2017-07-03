8197ea106c7a4344bbd5db63705382fd7e66b922
Anthony Whyte <anthwhyte@gmail.com>
2017-07-02 21:57:22 -0400

    Add ForumeventSubscribedTest

---
+++ src/test/java/org/imsglobal/caliper/events/ForumEventSubscribedTest.java
@@ -0,0 +1,201 @@
+/**
+ * This file is part of IMS Caliper Analytics™ and is licensed to
+ * IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+ * under one or more contributor license agreements.  See the NOTICE
+ * file distributed with this work for additional information.
+ *
+ * IMS Caliper is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.imsglobal.caliper.events;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import org.imsglobal.caliper.actions.Action;
+import org.imsglobal.caliper.context.JsonldContext;
+import org.imsglobal.caliper.context.JsonldStringContext;
+import org.imsglobal.caliper.databind.JxnCoercibleSimpleModule;
+import org.imsglobal.caliper.entities.agent.CourseSection;
+import org.imsglobal.caliper.entities.agent.Membership;
+import org.imsglobal.caliper.entities.agent.Person;
+import org.imsglobal.caliper.entities.agent.Role;
+import org.imsglobal.caliper.entities.agent.SoftwareApplication;
+import org.imsglobal.caliper.entities.agent.Status;
+import org.imsglobal.caliper.entities.resource.Forum;
+import org.imsglobal.caliper.entities.session.Session;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+
+@Category(org.imsglobal.caliper.UnitTest.class)
+public class ForumEventSubscribedTest {
+    private JsonldContext context;
+    private String id;
+    private Person actor;
+    private Forum object;
+    private SoftwareApplication edApp;
+    private CourseSection group;
+    private Membership membership;
+    private Session session;
+    private ForumEvent event;
+
+    private static final String BASE_IRI = "https://example.edu";
+    private static final String SECTION_IRI = BASE_IRI.concat("/terms/201601/courses/7/sections/1");
+
+    @Before
+    public void setUp() throws Exception {
+        context = JsonldStringContext.getDefault();
+
+        id = "urn:uuid:a2f41f9c-d57d-4400-b3fe-716b9026334e";
+
+        actor = Person.builder().id(BASE_IRI.concat("/users/554433")).build();
+
+        object = Forum.builder()
+            .id(SECTION_IRI.concat("/forums/1"))
+            .name("Caliper Forum")
+            .isPartOf(CourseSection.builder().id(SECTION_IRI).build())
+            .dateCreated(new DateTime(2016, 9, 14, 11, 0, 0, 0, DateTimeZone.UTC))
+            .build();
+
+        edApp = SoftwareApplication.builder().id(BASE_IRI.concat("/forums")).version("v2").build();
+
+        group = CourseSection.builder()
+            .id(BASE_IRI.concat("/terms/201601/courses/7/sections/1"))
+            .courseNumber("CPS 435-01")
+            .academicSession("Fall 2016")
+            .build();
+
+        membership = Membership.builder()
+            .id(BASE_IRI.concat("/terms/201601/courses/7/sections/1/rosters/1"))
+            .member(Person.builder().id(actor.getId()).coercedToId(true).build())
+            .organization(CourseSection.builder().id(group.getId()).coercedToId(true).build())
+            .status(Status.ACTIVE)
+            .role(Role.LEARNER)
+            .dateCreated(new DateTime(2016, 8, 1, 6, 0, 0, 0, DateTimeZone.UTC))
+            .build();
+
+        session = Session.builder()
+            .id(BASE_IRI.concat("/sessions/1f6442a482de72ea6ad134943812bff564a76259"))
+            .startedAtTime(new DateTime(2016, 11, 15, 10, 0, 0, 0, DateTimeZone.UTC))
+            .build();
+
+        // Build event
+        event = buildEvent(Action.SUBSCRIBED);
+    }
+
+    @Test
+    public void caliperEventSerializesToJSON() throws Exception {
+        SimpleFilterProvider provider = new SimpleFilterProvider()
+            .setFailOnUnknownId(true);
+
+        ObjectMapper mapper = new ObjectMapper()
+            .setDateFormat(new ISO8601DateFormat())
+            .setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
+            .setFilterProvider(provider)
+            .registerModules(new JodaModule(), new JxnCoercibleSimpleModule());
+
+        String json = mapper.writeValueAsString(event);
+
+        String fixture = jsonFixture("fixtures/caliperEventForumSubscribed.json");
+        JSONAssert.assertEquals(fixture, json, JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void messageEventRejectsStartedAction() {
+        buildEvent(Action.STARTED);
+    }
+
+    @After
+    public void teardown() {
+        event = null;
+    }
+
+    /**
+     * Build Media event.
+     * @param action
+     * @return event
+     */
+    private ForumEvent buildEvent(Action action) {
+        return ForumEvent.builder()
+            .context(context)
+            .id(id)
+            .actor(actor)
+            .action(action)
+            .object(object)
+            .eventTime(new DateTime(2016, 11, 15, 10, 16, 0, 0, DateTimeZone.UTC))
+            .edApp(edApp)
+            .group(group)
+            .membership(membership)
+            .session(session)
+            .build();
+    }
+}
+
+/**
+{
+    "@context": "http://purl.imsglobal.org/ctx/caliper/v1p1",
+    "id": "urn:uuid:a2f41f9c-d57d-4400-b3fe-716b9026334e",
+    "type": "ForumEvent",
+    "actor": {
+    "id": "https://example.edu/users/554433",
+    "type": "Person"
+    },
+    "action": "Subscribed",
+    "object": {
+    "id": "https://example.edu/terms/201601/courses/7/sections/1/forums/1",
+    "type": "Forum",
+    "name": "Caliper Forum",
+    "isPartOf": {
+    "id": "https://example.edu/terms/201601/courses/7/sections/1",
+    "type": "CourseSection"
+    },
+    "dateCreated": "2016-09-14T11:00:00.000Z"
+    },
+    "eventTime": "2016-11-15T10:16:00.000Z",
+    "edApp": {
+    "id": "https://example.edu/forums",
+    "type": "SoftwareApplication",
+    "version": "v2"
+    },
+    "group": {
+    "id": "https://example.edu/terms/201601/courses/7/sections/1",
+    "type": "CourseSection",
+    "courseNumber": "CPS 435-01",
+    "academicSession": "Fall 2016"
+    },
+    "membership": {
+    "id": "https://example.edu/terms/201601/courses/7/sections/1/rosters/1",
+    "type": "Membership",
+    "member": "https://example.edu/users/554433",
+    "organization": "https://example.edu/terms/201601/courses/7/sections/1",
+    "roles": [ "Learner" ],
+    "status": "Active",
+    "dateCreated": "2016-08-01T06:00:00.000Z"
+    },
+    "session": {
+    "id": "https://example.edu/sessions/1f6442a482de72ea6ad134943812bff564a76259",
+    "type": "Session",
+    "startedAtTime": "2016-11-15T10:00:00.000Z"
+    }
+    }
+
+    */
