23146739ddd773f9d32e9015d94217c5e576e838
Anthony Whyte <anthwhyte@gmail.com>
2018-03-26 16:37:09 -0600

    CourseSection extends CourseOffering.

--- src/main/java/org/imsglobal/caliper/entities/agent/CourseSection.java
+++ src/main/java/org/imsglobal/caliper/entities/agent/CourseSection.java
@@ -33,7 +33,7 @@ import javax.annotation.Nullable;
  * A Caliper CourseSection provides a subset of the CourseSection properties specified in the
  * IMS LTI 2.0 specification, which in turn, draws inspiration from the IMS LIS 1.0 specification.
  */
-public class CourseSection extends AbstractCourse {
+public class CourseSection extends CourseOffering {
 
     @JsonProperty("category")
     private final String category;
@@ -58,7 +58,7 @@ public class CourseSection extends AbstractCourse {
      * Builder class provides a fluid interface for setting object properties.
      * @param <T> builder.
      */
-    public static abstract class Builder<T extends Builder<T>> extends AbstractCourse.Builder<T> {
+    public static abstract class Builder<T extends Builder<T>> extends CourseOffering.Builder<T> {
         private String category;
 
         /**
