260ac94c935069d7dcb9989427164d6f801622a0
niteshkomalwar <nitesh.komalwar@imaginelearning.com>
2024-12-02 20:53:42 +0530

    CTCH-6534 Added new Action: Aliased Added a new event named ResourceManagementEvent

--- src/main/java/org/imsglobal/caliper/events/ResourceManagementEvent.java
+++ src/main/java/org/imsglobal/caliper/events/ResourceManagementEvent.java
@@ -25,32 +25,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SupportedActions({
-        Action.TAKE_LAUNCHED,
-        Action.TAKE_COMPLETED,
-        Action.SAVED,
-        Action.DELETED
+        Action.ALIASED,
+        Action.DELETED,
+        Action.SAVED
 })
-public class DesignerPathSaveEvent extends Event {
+public class ResourceManagementEvent extends Event {
 
     @JsonIgnore
-    private static final Logger log = LoggerFactory.getLogger(DesignerPathSaveEvent.class);
+    private static final Logger log = LoggerFactory.getLogger(ResourceManagementEvent.class);
 
     /**
-     * Utilize builder to construct DesignerPathSaveEvent
+     * Utilize builder to construct PathEvent
      *
      * @param builder
      */
-    protected DesignerPathSaveEvent(Builder<?> builder) {
+    protected ResourceManagementEvent(Builder<?> builder) {
         super(builder);
 
-        EventValidator.checkType(this.getType(), EventType.DESIGNER_PATH_SAVE);
-        EventValidator.checkAction(this.getAction(), DesignerPathSaveEvent.class);
+        EventValidator.checkType(this.getType(), EventType.RESOURCE_MANAGEMENT_EVENT);
+        EventValidator.checkAction(this.getAction(), ResourceManagementEvent.class);
 
     }
 
     /**
      * Initialize default parameter values in the builder.
-     * 
+     *
      * @param <T>
      *            builder
      */
@@ -60,16 +59,16 @@ public class DesignerPathSaveEvent extends Event {
          * Constructor
          */
         public Builder() {
-            super.type(EventType.DESIGNER_PATH_SAVE);
+            super.type(EventType.RESOURCE_MANAGEMENT_EVENT);
         }
 
         /**
          * Client invokes build method in order to create an immutable profile object.
-         * 
-         * @return a new DesignerPathSaveEvent instance.
+         *
+         * @return a new PathEvent instance.
          */
-        public DesignerPathSaveEvent build() {
-            return new DesignerPathSaveEvent(this);
+        public ResourceManagementEvent build() {
+            return new ResourceManagementEvent(this);
         }
     }
 
@@ -85,7 +84,7 @@ public class DesignerPathSaveEvent extends Event {
 
     /**
      * Static factory method.
-     * 
+     *
      * @return a new instance of the builder.
      */
     public static Builder<?> builder() {
